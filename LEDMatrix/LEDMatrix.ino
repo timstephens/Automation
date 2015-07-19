3d2e02dde2a0b45e62e666465407c8845c5f7a7e
Tim Stephens <tim@tjstephens.com>
2015-07-19 16:19:23 +0100

    Working display of millis() on ESP8266

--- LEDMatrix/LEDMatrix.ino
+++ LEDMatrix/LEDMatrix.ino
@@ -1,15 +1,25 @@
 #include "LedControl.h"
 #include "font.h"
+#include <WiFiClient.h>
+#include <ESP8266mDNS.h>
+
+
+const char* ssid = "virginmedia0465902";
+const char* password = "mqqvrjww";
+MDNSResponder mdns;
+
 
 #define ARRAYCOLS 128
 #define NUMARRAYS  8
 
-LedControl lc = LedControl(12, 11, 10, NUMARRAYS);
+LedControl lc = LedControl(14, 13, 12, NUMARRAYS); //12, 11, 10, NUMARRAYS);
 
 /* we always wait a bit between updates of the display */
 unsigned long delaytime = 500;
 
 void setup() {
+  Serial.begin(115200);
+  Serial.println("connected");
   /*
    The MAX72XX is in power-saving mode on startup,
    we have to do a wakeup call
@@ -23,23 +33,27 @@ void setup() {
     /* and clear the display */
     lc.clearDisplay(devices);
   }
-  Serial.begin(9600);
+ /* WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }*/
 }
 
 void printChar(String row1Data) {
-  
+
   /* ****************************************************
-  Build up the text that you want to write to the screen 
-  into a buffer, made up of 64 chars (corresponding to a 
+  Build up the text that you want to write to the screen
+  into a buffer, made up of 64 chars (corresponding to a
   column each).
-  Then write the buffer out to the display, reconfiguring 
+  Then write the buffer out to the display, reconfiguring
   it as appropriate for the display layout.
   **************************************************** */
-  
+
   char buf[ARRAYCOLS];
-  int c,i, count;
+  int c, i, count;
   int array, col;
-  
+
   //Initialise the buffer so it's all zeros when we begin
   memset(buf, 0, sizeof(buf));
 
@@ -47,7 +61,7 @@ void printChar(String row1Data) {
   //String row1Data = "test";
   //For each character
   for (int index = 0; index < row1Data.length(); index++) {
-   c = row1Data.charAt(index);
+    c = row1Data.charAt(index);
     for (int i = 0; i < 6; i++) { //For each column within a character
       buf[count] =  pgm_read_byte(&ASCII[(int)c][i]); //We're bit-shifting the data in one column at a time
       count++;
@@ -57,27 +71,25 @@ void printChar(String row1Data) {
   //There are 64 columns, so do a modulus%8 of the current index in the buffer. The mod is the array number and the remainder is the column.
   for (int i = 0; i < ARRAYCOLS; i++) {
     array = i / 8;
-    col = 7-(i % 8); //The column order is the reverse of the display order, so need to reverse it here by subtracting from 7.
-    //i.e. col7 needs to be sent as col 1, etc. 
+    col = 7 - (i % 8); //The column order is the reverse of the display order, so need to reverse it here by subtracting from 7.
+    //i.e. col7 needs to be sent as col 1, etc.
     lc.setColumn(array, col, buf[i]);
   }
 }
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  for (int i = 0; i < 94; i++)
-  {
-    //lc.setLed(3, i, i, true);
-    //  lc.setRow(2, 2, 0xFFFF);
-    //lc.setColumn(1, 2, 0xFF);
-    //lc.setLed(0, i, i, true);
-    
-    printChar(String(millis(), DEC));
-    delay(500);
-  }
+
+  //lc.setLed(3, i, i, true);
+  //  lc.setRow(2, 2, 0xFFFF);
+  //lc.setColumn(1, 2, 0xFF);
+  //lc.setLed(0, i, i, true);
+
+  printChar(String(millis(), DEC));
+
+  delay(500);
   for (int devices = 0; devices < NUMARRAYS; devices++) {
     lc.clearDisplay(devices);
   }
-
+  Serial.println("Main Loop");
 }
