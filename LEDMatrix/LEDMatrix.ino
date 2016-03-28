f216667e0019e7cfe417ecdeb370227d0db44768
Tim Stephens <tim@tjstephens.com>
2016-03-28 15:14:02 +0100

    Getting there
    LED Matrix displays millis()
    Connects to WiFi and MQTT broker, publishes and receives messages with
    confirmation through Serial connection

--- LEDMatrix/LEDMatrix.ino
+++ LEDMatrix/LEDMatrix.ino
@@ -2,7 +2,7 @@
 #include "font.h"
 #include <WiFiClient.h>
 #include <ESP8266mDNS.h>
-
+#include <PubSubClient.h>
 
 const char* ssid = "virginmedia0465902";
 const char* password = "mqqvrjww";
@@ -18,6 +18,50 @@ LedControl lc = LedControl(14, 13, 12, NUMARRAYS); //ESP03  Version
 
 /* we always wait a bit between updates of the display */
 unsigned long delaytime = 500;
+
+
+// Update these with values suitable for your network.
+byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
+//IPAddress ip(192.168.0.9);
+IPAddress server(192.168.0.8);
+//8*************
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+WiFiClient ethClient;
+PubSubClient client(ethClient);
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("arduinoClient")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic","hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+//***************************
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -40,6 +84,9 @@ void setup() {
      delay(500);
      Serial.print(".");
    }//*/
+   
+    client.setServer(server, 1883);
+  client.setCallback(callback);
 }
 
 void printChar(String row1Data) {
