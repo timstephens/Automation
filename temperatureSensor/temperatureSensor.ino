66553d52c90a2edec54f977f60f120cf252b75a4
Tim Stephens <tim@tjstephens.com>
2016-11-13 17:06:52 +0000

    Revert "Config server, and sleep"
    This reverts commit aa25cd490c7b3ed2b0cc843134b077d644cdfd3d.
    
    # Conflicts:
    #	temperatureSensor/temperatureSensor.ino

--- temperatureSensor/temperatureSensor.ino
+++ temperatureSensor/temperatureSensor.ino
@@ -1,7 +1,8 @@
 
 #include <WiFiClient.h>
-//#include <ESP8266mDNS.h>
+#include <ESP8266mDNS.h>
 #include <PubSubClient.h>
+//#include <DHT.h>
 #include <DallasTemperature.h>
 #include <OneWire.h>
 #include <ESP.h>
@@ -13,10 +14,9 @@
 ESP8266WebServer configServer(80);
 #define ssidAddr 0 //Locations in EEPROM memory where the SSID and Password will be stored. 
 #define passAddr 64
-#define IPAddr 256
 int serverStatus;
 
-
+//DHT sens1(2, DHT11);
 
 // Data wire is plugged into port 2 on the Arduino
 #define ONE_WIRE_BUS 2
@@ -25,22 +25,11 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 // Update these with values suitable for your network.
-
-IPAddress server(192, 168, 0, 8);  //Default
-String clientName; //global to store the name of this device (based on the MAC)
-
+byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
+//IPAddress ip(192.168.0.9);
+IPAddress server(192, 168, 0, 8);
 //8*************
 
-String macToStr(const uint8_t* mac)
-{
-  String result;
-  for (int i = 0; i < 6; ++i) {
-    result += String(mac[i], 16);
-    if (i < 5)
-      result += ':';
-  }
-  return result;
-}
 
 
 void handleRoot() {
@@ -49,7 +38,11 @@ void handleRoot() {
   String s;
   s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>Hello from ESP8266 at ";
   s += WiFi.softAPIP();
+<<<<<<< HEAD
   s += "<form method='get' action='a'><label>SSID: </label><input name='ssid' length=32><label>Pass: </label><input name='pass' length=64><br/><label>Broker IP: </label><input name='host1' length=4>.<input name='host2' length=4>.<input name='host3' length=4>.<input name='host4' length=4><input type='submit'></form>";  //This is a cheaty way to get the IP address, but I couldn't be bothered to get DNS lookups to work on the local network, so we'll have to assume that the MQTT broker remains on a static IP (which is valid whilst I control everything).
+=======
+  s += "<form method='get' action='a'><label>SSID: </label><input name='ssid' length=32><input name='pass' length=64><input type='submit'></form>";
+>>>>>>> parent of aa25cd4... Config server, and sleep
   s += "</html>\r\n\r\n";
   Serial.println("Sending 200");
   configServer.send(200, "text/html", s);
@@ -57,28 +50,15 @@ void handleRoot() {
 
 
 void handleSetup() {
-  String ssid, pass, host1, host2, host3, host4;
+  String ssid, pass;
   ssid = configServer.arg("ssid");
   pass = configServer.arg("pass");
-  host1 = configServer.arg("host1");
-  host2 = configServer.arg("host2");
-  host3 = configServer.arg("host3");
-  host4 = configServer.arg("host4");
 
   Serial.print("SSID is ");
   Serial.println(ssid);
   Serial.print("Password is ");
   Serial.println(pass);
-  Serial.print("Conecting to ");
-  Serial.print(host1);
-  Serial.print(".");
-  Serial.print(host2);
-  Serial.print(".");
-  Serial.print(host3);
-  Serial.print(".");
-  Serial.println(host4);
 
-  // IPAddress host(host1.toInt(), host2().toInt, host3.toInt(), host4.toInt());
   //Now need to set the SSID and password into the memory of the ESP, and make it reconnect to that.
   //There also needs to be some though about how we make it try to connect to the wifi first.
   for (int i = 0; i < ssid.length() + 1; ++i) {
@@ -90,10 +70,6 @@ void handleSetup() {
     EEPROM.write(passAddr + i, pass.c_str()[i]);
     Serial.print(pass.c_str()[i]);
   }
-  EEPROM.write(IPAddr, host1.toInt());
-  EEPROM.write(IPAddr+4, host2.toInt());
-  EEPROM.write(IPAddr+8, host3.toInt());
-  EEPROM.write(IPAddr+12, host4.toInt());
   Serial.println();
   EEPROM.commit();
 
@@ -120,17 +96,16 @@ WiFiClient ethClient;
 PubSubClient client(ethClient);
 
 void reconnect() {
-
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect(clientName.c_str())) {
+    if (client.connect("arduinoClient")) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish("/control", clientName.c_str());
+      client.publish("/control", "WeatherBug!");
       // ... and resubscribe
-      client.subscribe("/control");
+      client.subscribe("/inWeather");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -146,18 +121,11 @@ void reconnect() {
 
 
 void setup() {
-  int host1, host2, host3, host4; //the IP addresses of the mqtt broker.
   serverStatus = 0; //This is the default case, where there is no server started for config.
   Serial.begin(115200);
   Serial.println("connected");
 
-  //Generate the MQTT clientname from the MAC Address.
-  uint8_t mac[6];
-  WiFi.macAddress(mac);
-  clientName += "TempSensor-";
-  clientName += macToStr(mac);
-
-  EEPROM.begin(512); //Begin the EEPROM session
+  EEPROM.begin(255); //Begin the EEPROM session
 
   Serial.println("Values stored in EEPROM are:");
   Serial.println("Reading EEPROM ssid");
@@ -177,19 +145,10 @@ void setup() {
   Serial.print("PASS: ");
   Serial.println(epass);
 
-  host1 = EEPROM.read(IPAddr);  //Get the IP Address. 
-  host2 = EEPROM.read(IPAddr + 4);
-  host3 = EEPROM.read(IPAddr + 8);
-  host4 = EEPROM.read(IPAddr + 12);
-
-  server = IPAddress(host1, host2, host3, host4);
-  Serial.print("Broker IP=");
-  Serial.println(server);
-  WiFi.hostname(clientName.c_str());  //Set the hostname of the wifi device so it appears names after the MAC on the setup page of routers etc.
   WiFi.begin(esid.c_str(), epass.c_str());
   int connectCount = 0;
   while ((WiFi.status() != WL_CONNECTED) && connectCount < 20) { //Should try to connect for ~30s
-    delay(2000);
+    delay(500);
     Serial.print("*");
     connectCount ++;
   }
@@ -198,11 +157,9 @@ void setup() {
     //Need to serve up a page that offers the opportunity to enter server details etc.
     Serial.println("Failed to connect to WiFi netowrk.");
     Serial.println("Change to configuration mode");
-
     // Start the server
-
     Serial.print("Setting soft-AP ... ");
-    Serial.println(WiFi.softAP(clientName.c_str()) ? "Ready" : "Failed!");  //clientName is generated in Setup and should be unique to the device since it's based on the MAC.
+    Serial.println(WiFi.softAP("ESPsoftAP_012") ? "Ready" : "Failed!");
     Serial.println(WiFi.softAPIP());
 
     configServer.on("/", handleRoot);
@@ -220,7 +177,9 @@ void setup() {
   }
   //
 
+  //
   //  //Start the temperature sensor
+  //    sens1.begin();
 
   sensors.begin();
 }
@@ -229,18 +188,29 @@ void setup() {
 void loop() {
   Serial.printf("Stations connected = %d\n", WiFi.softAPgetStationNum());
 
-  if (serverStatus == 0) {  //Only want to handle the webserver stuff if it's running.
+  if (serverStatus == 0) {
     char buffer[10];
+    //  float t1, h1;
+    //    t1 = 4.56; //sens1.readTemperature();
+    //    h1 = 5.46; //sens1.readHumidity();
+    //    String tempF = String(t1, DEC); //dtostrf(t1, 4, 1, buffer); //
+    //    tempF += " ";
+    //    tempF  += String(h1, DEC); //dtostrf(h1, 4, 1, buffer); //
+    //
+    //  for (int myloop = 0; myloop < 5; myloop++) {
     sensors.requestTemperatures();
-    // After we get the temperatures, we can print them here.
+    // After we got the temperatures, we can print them here.
     // We use the function ByIndex, and as an example get the temperature from the first sensor only.
     Serial.print("Temperature for the device 1 (index 0) is: ");
     Serial.println(sensors.getTempCByIndex(0));
+    delay(2000);
     String tempC = String(sensors.getTempCByIndex(0));
 
+    Serial.println("Main Loop");
     if (!client.connected()) {
       reconnect();
     } else {
+<<<<<<< HEAD
       Serial.println("Publishing");
       String publishTopic = "/temperature/";
       publishTopic += clientName; 
@@ -251,11 +221,16 @@ void loop() {
       //Serial.println("Done, Sleeping 10s");
 
       //ESP.deepSleep(10000000, WAKE_RF_DEFAULT);
+=======
+      client.publish("/outTemp", (char *)tempC.c_str());
+      Serial.println("Sleeping 300s");
+      ESP.deepSleep(300000000, WAKE_RF_DEFAULT);
+>>>>>>> parent of aa25cd4... Config server, and sleep
     }
     client.loop();
-    
   } else {
     configServer.handleClient();
   }
-  delay(2000);
+  //ESP.deepSleep(10000000,WAKE_RF_DEFAULT);
+  //  }
 }
